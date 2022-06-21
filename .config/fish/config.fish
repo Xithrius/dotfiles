fe9f04753f2de57c5a614670d9b59838f631ae86
Xithrius <xithrius@gmail.com>
2022-06-20 19:25:30 -0700

    Added even more aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive && not set -q TMUX
     exec tmux
 end
 
-set -gx PATH $HOME/.cargo/bin $HOME/.donet/tools $PATH
+set -gx PATH $HOME/.local/bin $HOME/.cargo/bin $HOME/.donet/tools $PATH
 setenv SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 setenv GPG_TTY (tty)
 setenv SSH_ASKPASS ssh-askpass
@@ -23,7 +23,18 @@ alias pls="sudo"
 alias py="python3"
 alias lg="lazygit"
 alias ld="lazydocker"
+
+
+# +-------------+
+# |  Not short  |
+# +-------------+
+
+alias weather="curl wttr.in"
+alias moon="curl wttr.in/moon"
 alias h="history | wc -l"
+alias xkcd="xdg-open \"https://xkcd.com/\""
+alias ytdl="youtube-dl -o './%(title)s.mp3' -f 'bestaudio/best' --yes-playlist"
+alias ytdl_vid="youtube-dl -o './%(title)s' --yes-playlist"
 
 
 # +-------+
@@ -49,6 +60,7 @@ alias killorphans="sudo pacman -Rnu (pacman -Qtdq)"
 alias hibernate="systemctl hibernate"
 alias zzz="systemctl suspend"
 alias howold="echo Install started on (head -1 /var/log/pacman.log | cut -d ' ' -f 1)"
+alias netscan='nmap -sn 192.168.1.0/24'
 
 
 # +-------------+
